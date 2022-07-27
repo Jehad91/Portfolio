cf78d434e0fcfd7abb4c4ca809fe78764580b733
Jehad91 <j.abushaqra1@gmail.com>
2022-07-27 21:03:10 +0300

    create home page

---
+++ src/Components/CV.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Anchor } from 'grommet';
+
+const CV = () => {
+  return (
+    <Anchor
+      href="https://docdro.id/HVkhZg2"
+      target="_blank"
+      color="#000"
+      style={{
+        position: "absolute",
+        top: "30px",
+        right: "30px"
+      }}
+    >
+      CV
+    </Anchor>
+  )
+}
+
+export default CV;
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Grommet } from 'grommet';
+
+const theme = {
+  global: {
+    colors: {
+      brand: '#228BE6',
+    },
+    font: {
+      family: 'system-ui',
+      size: '20px',
+    },
+  },
+};
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
   );
-root.render(<App/>);
+root.render(
+  <Grommet theme={theme}>
+    <App/>
+  </Grommet>
+);
 serviceWorker.unregister();
