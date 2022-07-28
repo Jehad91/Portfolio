869cc8e08de1ea060043c3f19e5feb3b056c7eb7
Jehad91 <j.abushaqra1@gmail.com>
2022-07-28 15:22:40 +0300

    fix particles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import React from "react";
-import ReactFullpage from '@fullpage/react-fullpage';
+import React, { useCallback, useState } from 'react';
+import Particles from "react-tsparticles";
+import type { Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
+import particlesOptionsDark from "./Config/particles-dark.json";
+import particlesOptionsLight from "./Config/particles-light.json";
+import { ISourceOptions } from "tsparticles-engine";
+import ReactFullpage, { Item } from '@fullpage/react-fullpage';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Skills from './Pages/Skills';
@@ -7,11 +13,20 @@ import Contact from './Pages/Contact';
 import Works from './Pages/Works';
 import './App.css';
 
+
 const App = () => {
+  const [page, setPage] = useState<String | Number>('');
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+  console.log(page)
   return (
+    <>
+      <Particles options={page === 'about' ? particlesOptionsLight as ISourceOptions : particlesOptionsDark as ISourceOptions} init={particlesInit} />
       <ReactFullpage
         licenseKey = {'YOUR_KEY_HERE'}
         scrollingSpeed = {1000}
+        anchors= {['home', 'about', 'skills', 'works', 'contact']}
         // sectionsColor = {['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90']}
         navigation = {true}
         navigationPosition = {'right'}
@@ -20,8 +35,13 @@ const App = () => {
         slidesNavigation = {true}
         slidesNavPosition = {'bottom'}
         lazyLoading = {true}
-      
+        afterLoad= {function(origin:Item, destination:Item, direction:string){
+          console.log(destination);
+          setPage(destination.anchor);
+        }}
+        
         render={() => {
+          
           return (
             <ReactFullpage.Wrapper>
               <div className="section">
@@ -43,6 +63,7 @@ const App = () => {
           );
         }}
       />
+    </>
   );
 }
 
--- src/Pages/Skills.tsx
+++ src/Pages/Skills.tsx
@@ -1,21 +1,11 @@
-import React, { useCallback } from 'react';
-import Particles from "react-tsparticles";
-import type { Engine } from "tsparticles-engine";
-import { loadFull } from "tsparticles";
-import particlesOptions from "../particles.json";
-import { ISourceOptions } from "tsparticles-engine";
+import React from 'react';
 
 const Skills = () => {
 
-  const particlesInit = useCallback(async (engine: Engine) => {
-    await loadFull(engine);
-  }, []);
-
   return (
-    <div>
-      <Particles options={particlesOptions as ISourceOptions} init={particlesInit} />
+    <>
       <h1>Skills</h1>
-    </div>
+    </>
   )
 }
 
